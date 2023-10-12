09a646c44281ddec128bf2806bf276ae8ebbf03f
chaoditang <61668922+chaoditang@users.noreply.github.com>
2023-10-12 22:47:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chaodi Tang's Blog World',
+  domain: 'chaodit.tech',
+  author: 'Chaodi Tang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My Blogs.',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
